50c2a9d62b7a188f15d0b5c9d73815cf8768fcd6
Riccardo <riki.brero@gmail.com>
2022-10-15 01:48:55 +0200

    Update base_colors.dart

--- packages/bds_colors/lib/base_colors.dart
+++ packages/bds_colors/lib/base_colors.dart
@@ -4,7 +4,8 @@ import 'package:bds_colors/color_palette.dart';
 import 'package:bds_colors/generated/palettes.g.dart';
 
 class BaseColors {
-  void test() {
+  Color test() {
     Color color = Palettes.amber.get(Shade.shade1, brightness: Brightness.dark);
+    return color;
   }
 }
